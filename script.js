e62054cb7e5a7be17e0551a8b018eab8bec99041
yaminmhd <mdyamin.drzee@gmail.com>
2018-03-07 20:20:26 +0800

    Updated changes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 tasks = ["buy milk", "eat dinner", "nail javascript"]
 
+
 //declare elements
 var todoList = document.getElementById('todo-list');
-var addButton = document.getElementById('addBtn');
-
 
 function strike(e){
   if(e.target.classList.contains('done')){
@@ -13,6 +12,10 @@ function strike(e){
   }
 };
 
+function clearContent(){
+  input.value = '';
+}
+
 function addNewItem(todoContent){
   var item = document.createElement('li');
   item.textContent = todoContent;
@@ -20,43 +23,19 @@ function addNewItem(todoContent){
   todoList.appendChild(item);
 }
 
-function clearContent(){
-  input.value = '';
-}
-
-
+//display existing todos
 for(var i = 0; i<tasks.length;i++){
   addNewItem(tasks[i]);
 }
 
-//function for adding item to list
-
+//function for adding item to list using input
 var form = document.getElementsByTagName('form')[0];
-var input = document.getElementById('input');
+//var input = document.getElementById('input');
+
 function addItem(e){
-  if(e.charCode === 13){
     e.preventDefault();
     addNewItem(input.value);
     clearContent();
-  }else{
-    console.log('Other keys pressed');
-  }
 }
 
-//function for adding item to list using enter key
-// function addItemUsingEnter(e){
-//   if(e.charCode === 13) {
-//     e.preventDefault();
-//     var inputValue = form.elements['input'].value;
-//     var item = document.createElement('li');
-//     item.textContent = inputValue;
-//     todoList.appendChild(item);
-//     item.addEventListener('click', strike);
-//     clearContent();
-//   }else{
-//     console.log('Press enter to submit!!!');
-//   }
-// }
-
-//form.addEventListener('submit', addItem);
-form.addEventListener('keypress', addItem);
+form.addEventListener('submit', addItem);
